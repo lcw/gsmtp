c1959047b39d87d2a170f68c2c3de60a3fba9357
Lucas C Wilcox <lucas@swirlee.com>
2017-08-02 12:32:30 -0700

    Add print server functionality

--- gsmtp.go
+++ gsmtp.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"crypto/sha256"
+	"crypto/tls"
+	"encoding/pem"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
+	"net"
+	"net/smtp"
 	"os"
 	"path"
 	"runtime"
@@ -32,12 +37,69 @@ var defaultConfigFile = path.Join(userHomeDir(), ".config", "gsmtp", "init.toml"
 var configFile = flag.String("config", defaultConfigFile,
 	"File to read configuration from")
 var debug = flag.Bool("debug", false, "Verbose")
+var serverinfo = flag.Bool("serverinfo", false, "Print server info and quit")
 var _ = flag.Bool("oi", false, "Ignored sendmail flag")
 
 type server struct {
 	Addr string `toml:"address,omitempty"`
 }
 type servers map[string]server
+
+func printServerInfo(config servers) {
+	for name, s := range config {
+		fmt.Printf("\n------------------------------------------------------------------------\n")
+		fmt.Printf("  Server info for: %s\n", name)
+		fmt.Printf("------------------------------------------------------------------------\n")
+
+		host, _, err := net.SplitHostPort(s.Addr)
+		if err != nil {
+			log.Panic(err)
+		}
+
+		tlsconfig := &tls.Config{
+			InsecureSkipVerify: true,
+			ServerName:         host,
+		}
+
+		c, err := smtp.Dial(s.Addr)
+		if err != nil {
+			log.Panic(err)
+		}
+
+		c.StartTLS(tlsconfig)
+
+		state, ok := c.TLSConnectionState()
+		if !ok {
+			log.Panic("STARTTLS Failed")
+		}
+
+		for _, cert := range state.PeerCertificates {
+			for i, dnsname := range cert.DNSNames {
+				fmt.Printf("DNSnames[%d]: %s\n", i, dnsname)
+			}
+			for i, value := range cert.CRLDistributionPoints {
+				fmt.Printf("CRLDistributionPoints[%d]: %s\n", i, value)
+			}
+			for i, value := range cert.IssuingCertificateURL {
+				fmt.Printf("IssuingCertificateURL[%d]: %s\n", i, value)
+			}
+			hash := sha256.New()
+			hash.Write(cert.Raw)
+			fingerprint := fmt.Sprintf("%X", hash.Sum(nil))
+			fmt.Printf("SHA-256 Fingerprint:\n %s\n", fingerprint)
+
+			pemBlock := pem.Block{
+				Type:  "CERTIFICATE",
+				Bytes: cert.Raw,
+			}
+			fmt.Print(string(pem.EncodeToMemory(&pemBlock)))
+
+			fmt.Printf("\n")
+		}
+
+		fmt.Printf("------------------------------------------------------------------------\n\n\n")
+	}
+}
 
 func main() {
 	flag.Parse()
@@ -68,6 +130,11 @@ func main() {
 			fmt.Printf("  Server: %s (addr: %s)\n", name, s.Addr)
 		}
 		fmt.Printf("\n")
+	}
+
+	if *serverinfo {
+		printServerInfo(config)
+		os.Exit(0)
 	}
 
 	os.Exit(1)
